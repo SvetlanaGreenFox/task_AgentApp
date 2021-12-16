5897222c0a79cc7d1bdc9c8c25d11f396088dcd4
Светлана <svetlanagreenfox@gmail.com>
2021-12-16 12:35:24 +0300

    Added html css

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataComponent } from './data/data.component';
+import { ButtonComponent } from './button/button.component';
+import { TransportersComponent } from './transporters/transporters.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataComponent,
+    ButtonComponent,
+    TransportersComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
